a19846510530ddd998a7a46d208940c11bd415a1
pjeet1507 <122104759+pjeet1507@users.noreply.github.com>
2023-12-03 05:09:18 -0500

    home page (basic work done)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
-document
-  .querySelector(".hamburger-menu")
-  .addEventListener("click", function () {
-    this.classList.toggle("change");
+(() => {
+  gsap.registerPlugin(ScrollTrigger);
+  // mobile-navigation
 
-    let target = ".mobile-nav-links";
-    if (this.classList.contains("change")) {
-      // Slide in from the right
-      gsap.to(target, { duration: 0.3, right: "0%", ease: "power3.out" });
-    } else {
-      // Slide out to the right
-      gsap.to(target, { duration: 0.3, right: "-100%", ease: "power3.in" });
-    }
+  document
+    .querySelector(".hamburger-menu")
+    .addEventListener("click", function () {
+      this.classList.toggle("change");
+
+      let target = ".mobile-nav-links";
+      if (this.classList.contains("change")) {
+        gsap.to(target, { duration: 0.3, right: "0%", ease: "power3.out" });
+      } else {
+        gsap.to(target, { duration: 0.3, right: "-100%", ease: "power3.in" });
+      }
+    });
+  //---------------------
+
+  // scroll triger [background color]
+
+  const colors = ["#7FBFD2", "#B8A2FE", "#0000ff"];
+  const sections = gsap.utils.toArray("section");
+
+  sections.forEach((section, index) => {
+    ScrollTrigger.create({
+      trigger: section,
+      //   markers: true,
+      start: "top center",
+      end: "bottom center",
+      toggleActions: "play none none reverse",
+      onEnter: () =>
+        gsap.to("body", { backgroundColor: colors[index], duration: 1 }),
+      onLeaveBack: () =>
+        gsap.to("body", {
+          backgroundColor: colors[index - 1] || "white",
+          duration: 1,
+        }),
+      onEnterBack: () =>
+        gsap.to("body", { backgroundColor: colors[index], duration: 1 }),
+    });
   });
+  //---------------------------------
+})();
