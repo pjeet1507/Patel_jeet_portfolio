1d665ab36ae59ddd906ab5afbec20951d857e6d0
pjeet1507 <122104759+pjeet1507@users.noreply.github.com>
2023-12-10 08:38:46 -0500

    video part done

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
 
   // scroll triger [background color]
 
-  const colors = ["#7FBFD2", "#B8A2FE", "#6dcc7a", "#eb4d55"];
+  const colors = ["#7FBFD2", "#B8A2FE", "#967BB6", "#6dcc7a", "#eb4d55"];
   const sections = gsap.utils.toArray("section");
 
   sections.forEach((section, index) => {
@@ -55,4 +55,74 @@
     delay: 0.7,
   });
   //---------------------
+
+  // variables
+
+  const playerCon = document.querySelector("#player-container");
+  const player = document.querySelector("video");
+  const videoControls = document.querySelector("#video-controls");
+  const playButton = document.querySelector("#play-button");
+  const pauseButton = document.querySelector("#pause-button");
+  const stopButton = document.querySelector("#stop-button");
+  const volumeSlider = document.querySelector("#change-vol");
+  const fullScreen = document.querySelector("#full-screen");
+
+  // functions
+
+  //if JS is loaded, super. Remove the default controls
+  player.controls = false;
+  videoControls.classList.remove("hidden");
+
+  function playVideo() {
+    player.play();
+  }
+
+  function pauseVideo() {
+    player.pause();
+  }
+
+  function stopVideo() {
+    player.pause();
+    player.currentTime = 1;
+  }
+
+  function changeVolume() {
+    player.volume = volumeSlider.value;
+    console.log(volumeSlider.value);
+  }
+
+  function toggleFullScreen() {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else if (document.webkitFullScreen) {
+      document.webkitFullScreen();
+    } else if (playerCon.webkitRequestFullScreen) {
+      playerCon.webkitRequestFullScreen();
+    } else {
+      playerCon.requestFullscreen();
+    }
+  }
+
+  function hideControls() {
+    if (player.paused) {
+      return;
+    }
+    videoControls.classList.add("hide");
+  }
+
+  function showControls() {
+    videoControls.classList.remove("hide");
+  }
+
+  // Event Listeners
+
+  playButton.addEventListener("click", playVideo);
+  pauseButton.addEventListener("click", pauseVideo);
+  stopButton.addEventListener("click", stopVideo);
+  volumeSlider.addEventListener("change", changeVolume);
+  fullScreen.addEventListener("click", toggleFullScreen);
+  videoControls.addEventListener("mouseenter", showControls);
+  videoControls.addEventListener("mouseleave", hideControls);
+  player.addEventListener("mouseenter", showControls);
+  player.addEventListener("mouseleave", hideControls);
 })();
